17ad3be5ad166c81e22aff0baa9241fe6b2c3fc4
deepesh <deepesh.krishnan@gmail.com>
2015-05-25 11:38:05 +0530

    Uodate media

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -8,7 +8,9 @@ class Medium < ActiveRecord::Base
 
    belongs_to :media_commision,  foreign_key: "media_commision_id"
    belongs_to :media_group,  foreign_key: "media_group_id"
-  
+    validates_numericality_of :value, allow_nil: true , numericality: { only_integer: true }, :less_than_or_equal_to => 1
+    validates_numericality_of :daily_charges, numericality: { only_integer: true }, allow_nil: true
+    validates_numericality_of :paid_correction, allow_nil: true , numericality: { only_integer: true }, :less_than_or_equal_to => 1
    def mediainfo
       if self.media_group_id.present?
           (self.media_group.name || "") + "--" + (self.name || "") + " -- " + (self.dnis || "") + " -- " + (self.telephone || "" ) + " -- " + self.state ||= 'All States'
