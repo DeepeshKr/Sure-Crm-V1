42baf9400ef8198f38f0b05721d72cb8d884e61a
deepesh <deepesh.krishnan@gmail.com>
2015-10-20 16:09:50 +0530

    media tapes updated

--- app/controllers/media_tapes_controller.rb
+++ app/controllers/media_tapes_controller.rb
@@ -162,7 +162,7 @@ class MediaTapesController < ApplicationController
       params.require(:media_tape).permit(:name,:release_date, :duration_secs, 
         :frames, :tape_ext_ref_id, :unique_tape_name, 
         :media_id, :product_variant_id, :description, :file_parts, 
-        :file_extenstion, :media_tape_head_id, :sort_order)
+        :file_extenstion, :media_tape_head_id, :sort_order, :frames)
 
     end
     def last_ext_tape_id
