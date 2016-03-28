6866d703e962fbdc8ff7f337606a6b7063cd08b6
Deepesh Krishnan <deepesh.krishnan@gmail.com>
2016-03-28 15:23:26 +0530

    updated with latest

--- app/controllers/return_rates_controller.rb
+++ app/controllers/return_rates_controller.rb
@@ -5,7 +5,9 @@ class ReturnRatesController < ApplicationController
   # GET /return_rates
   # GET /return_rates.json
   def index
-    @return_rates = ReturnRate.all
+    @return_rates_0 = ReturnRate.where("media_id is null and product_list_id is null").order(:no_of_days).where(offset: 0)
+    @return_rates_30 = ReturnRate.where("media_id is null and product_list_id is null").order(:no_of_days).where(offset: 30)
+    @return_rates_60 = ReturnRate.where("media_id is null and product_list_id is null").order(:no_of_days).where(offset: 60)
   end
 
   # GET /return_rates/1
@@ -31,9 +33,16 @@ class ReturnRatesController < ApplicationController
     def set_return_rate
       @return_rate = ReturnRate.find(params[:id])
     end
-
+    # t.integer  "shipped",            precision: 38
+    # t.integer  "offset",             precision: 38
+    # t.integer  "media_id",           precision: 38
+    # t.integer  "product_master_id",  precision: 38
+    # t.integer  "product_variant_id", precision: 38
+    # t.integer  "product_list_id",    precision: 38
+    # t.string   "ext_prod_code"
     # Never trust parameters from the scary internet, only allow the white list through.
     def return_rate_params
-      params.require(:return_rate).permit(:name, :sort_order, :total, :cancelled, :returned, :paid, :transfer_total, :transfer_paid, :transfer_cancelled, :no_of_days)
+      params.require(:return_rate).permit(:name, :sort_order, :total, :cancelled, :returned, :paid, :transfer_total, :transfer_paid, :transfer_cancelled, :no_of_days, :shipped, :offset, :media_id,
+      :product_master_id, :product_variant_id, :product_list_id, :ext_prod_code)
     end
 end
